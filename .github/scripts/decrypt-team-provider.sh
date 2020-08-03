cca7ac53f8ad4d17dbe1736d9856fc3688a8370e
Perttu Lähteenlahti <perttu.lahteenlahti@qvik.fi>
2020-08-03 18:45:27 +0300

    Build process

--- .github/scripts/decrypt-team-provider.sh
+++ .github/scripts/decrypt-team-provider.sh
@@ -5,4 +5,4 @@ mkdir $HOME/
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$PASS_PHRASE" \
---output ./amplify/team-provider-info.json .team/team-provider-info.json.gpg
+--output ./amplify/team-provider-info.json team/team-provider-info.json.gpg
