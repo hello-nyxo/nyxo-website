5327be3c8c37385e9e1be67d5b847730dd5c2207
Perttu Lähteenlahti <perttu.lahteenlahti@qvik.fi>
2020-08-06 19:11:37 +0300

    Maybe it'll start working

--- src/lib/createPages.ts
+++ src/lib/createPages.ts
@@ -1,56 +1,20 @@
 import { kebabCase } from "lodash"
 import path from "path"
 import {
-  ContentfulLesson,
-  ContentfulWeek,
-  ContentfulHabit,
-  ContentfulQuestionnaire,
   ContentfulAuthor,
+  ContentfulHabit,
+  ContentfulLesson,
+  ContentfulQuestionnaire,
+  ContentfulWeek,
 } from "../../graphql-types"
+import { getLocalizedPath } from "../Helpers/i18n-helpers"
 import { GatsbyCreatePages } from "../types"
 import markdownQuery from "./query-blog-posts"
-import {
-  authorsQuery,
-  contentfulTagsQuery,
-  habitsQuery,
-  lessonsQuery,
-  questionnairesQuery,
-  weeksQuery,
-} from "./query-contentful"
-import { getLocalizedPath } from "../Helpers/i18n-helpers"
-interface Post {
-  node: {
-    fields: {
-      slug: string
-    }
-    frontmatter: {
-      tags: string
-    }
-  }
-}
+import { contentfulData } from "./query-contentful"
+// import { BlogPostNode } from "../typings/blog-types"
 
-interface Week {
-  node: ContentfulWeek
-}
-
-interface Lesson {
-  node: ContentfulLesson
-}
-
-interface Habit {
-  node: ContentfulHabit
-}
-
-interface Questionaire {
-  node: ContentfulQuestionnaire
-}
-
-interface Author {
-  node: ContentfulAuthor
-}
-
-interface Questionnaire {
-  node: ContentfulQuestionnaire
+type ContentfulTag = {
+  fieldValue: string
 }
 
 export const createPages: GatsbyCreatePages = async ({
@@ -61,178 +25,139 @@ export const createPages: GatsbyCreatePages = async ({
   const { createPage } = boundActionCreators
 
   const allMarkdown = await graphql(markdownQuery)
+  const {
+    data: {
+      allContentfulWeek: { nodes: weeks },
+      allContentfulLesson: { nodes: lessons },
+      allContentfulHabit: { nodes: habits },
+      allContentfulAuthor: { nodes: authors },
+      allContentfulQuestionnaire: { nodes: questionnaires },
+      contentfulTagsQuery: { group: contentfulTags },
+    },
+  } = await graphql(contentfulData)
 
   if (allMarkdown.errors) {
     throw allMarkdown.errors
   }
 
-  const allWeeks = await graphql(weeksQuery)
-  const allLessons = await graphql(lessonsQuery)
-  const allHabits = await graphql(habitsQuery)
-  const allAuthors = await graphql(authorsQuery)
-  const allContentfulTags = await graphql(contentfulTagsQuery)
-  const allQuestionnaires = await graphql(questionnairesQuery)
-
-  if (allWeeks.errors) {
-    throw allWeeks.errors
-  }
-
-  if (allLessons.errors) {
-    throw allLessons.errors
-  }
-
-  if (allHabits.errors) {
-    throw allHabits.errors
-  }
-
-  const posts = allMarkdown.data.allBlogPosts.edges
-
-  /* WEEKS */
-  const weeks = allWeeks.data.allContentfulWeek.edges
-
-  /* LESSONS */
-  const lessons = allLessons.data.allContentfulLesson.edges
-
-  /* HABITS */
-  const habits = allHabits.data.allContentfulHabit.edges
-
-  /* AUTHORS */
-  const authors = allAuthors.data.allContentfulAuthor.edges
-  const questionnaires = allQuestionnaires.data.allContentfulQuestionnaire.edges
+  const posts = allMarkdown.data.allBlogPosts.nodes
   /* TAGS */
   const tags = allMarkdown.data.tagsGroup.group
-  const contentfulTags = allContentfulTags.data.contentfulTagsQuery.group
-
-  /* QUESTIONNAIRE */
 
   posts.forEach((post: any, index: number) => {
-    const previous =
-      index === posts.length - 1 ? null : posts[index + 1].node.slug
-    const next = index === 0 ? null : posts[index - 1].node.slug
+    const previous = index === posts.length - 1 ? null : posts[index + 1].slug
+    const next = index === 0 ? null : posts[index - 1].slug
     createPage({
-      path: post.node.fields.slug,
+      path: post.fields.slug as string,
       component: path.resolve(`./src/templates/blog-post.tsx`),
       context: {
         next,
         previous,
-        author: post.node.frontmatter.authorSlug,
-        slug: post.node.fields.slug,
+        author: post.frontmatter.authorSlug,
+        slug: post.fields.slug,
       },
     })
   })
 
-  tags.forEach((tag: any) => {
+  const allTags = [
+    ...new Set([
+      ...tags.map((tag: ContentfulTag) => tag.fieldValue),
+      ...contentfulTags.map((tag: ContentfulTag) => tag.fieldValue),
+    ]),
+  ]
+
+  allTags.forEach((tag: string) => {
     createPage({
-      path: `/tags/${kebabCase(tag.fieldValue)}/`,
+      path: `/tags/${kebabCase(tag)}/`,
       component: path.resolve(`./src/templates/tag.tsx`),
       context: {
-        tag: tag.fieldValue,
+        tag: tag,
       },
     })
   })
 
-  // eslint-disable-next-line sonarjs/no-identical-functions
-  contentfulTags.forEach((tag: any) => {
-    createPage({
-      path: `/tags/${kebabCase(tag.fieldValue)}/`,
-      component: path.resolve(`./src/templates/tag.tsx`),
-      context: {
-        tag: tag.fieldValue,
-      },
-    })
-  })
-
-  weeks.forEach((week: Week, index: number) => {
+  weeks.forEach((week: ContentfulWeek, index: number) => {
     const previousWeek =
-      index === weeks.length - 1 ? null : weeks[index + 1].node.slug
-    const nextWeek = index === 0 ? null : weeks[index - 1].node.slug
+      index === weeks.length - 1 ? null : weeks[index + 1].slug
+    const nextWeek = index === 0 ? null : weeks[index - 1].slug
 
     createPage({
-      path: getLocalizedPath(`/week/${week.node.slug}`, week.node.node_locale),
+      path: getLocalizedPath(`/week/${week.slug}`, week.node_locale),
       component: path.resolve(`./src/templates/week.tsx`),
       context: {
-        slug: week.node.slug,
-        locale: week.node.node_locale,
+        slug: week.slug,
+        locale: week.node_locale,
         next: nextWeek,
         previous: previousWeek,
       },
     })
   })
 
-  lessons.forEach((lesson: Lesson, index: number) => {
+  lessons.forEach((lesson: ContentfulLesson, index: number) => {
     const previousLesson =
-      index === lessons.length - 1 ? null : lessons[index + 1].node.slug
-    const nextLesson = index === 0 ? null : lessons[index - 1].node.slug
+      index === lessons.length - 1 ? null : lessons[index + 1].slug
+    const nextLesson = index === 0 ? null : lessons[index - 1].slug
 
     createPage({
-      path: getLocalizedPath(
-        `/lesson/${lesson.node.slug}`,
-        lesson.node.node_locale
-      ),
+      path: getLocalizedPath(`/lesson/${lesson.slug}`, lesson.node_locale),
       component: path.resolve(`./src/templates/lesson.tsx`),
       context: {
-        slug: lesson.node.slug,
-        locale: lesson.node.node_locale,
+        slug: lesson.slug,
+        locale: lesson.node_locale,
         previous: previousLesson,
         next: nextLesson,
       },
     })
   })
 
-  habits.forEach((habit: Habit, index: number) => {
+  habits.forEach((habit: ContentfulHabit, index: number) => {
     const previousHabit =
-      index === habits.length - 1 ? null : habits[index + 1].node.slug
-    const nextHabit = index === 0 ? null : habits[index - 1].node.slug
+      index === habits.length - 1 ? null : habits[index + 1].slug
+    const nextHabit = index === 0 ? null : habits[index - 1].slug
 
     createPage({
-      path: getLocalizedPath(
-        `/habit/${habit.node.slug}`,
-        habit.node.node_locale
-      ),
+      path: getLocalizedPath(`/habit/${habit.slug}`, habit.node_locale),
       component: path.resolve(`./src/templates/habit.tsx`),
       context: {
-        locale: habit.node.node_locale,
-        slug: habit.node.slug,
+        locale: habit.node_locale,
+        slug: habit.slug,
         next: nextHabit,
         previous: previousHabit,
       },
     })
   })
 
-  questionnaires.forEach((questionnaire: Questionnaire, index: number) => {
-    const previousQuestionnaire =
-      index === lessons.length - 1 ? null : lessons[index + 1].node.slug
-    const nextQuestionnaire = index === 0 ? null : lessons[index - 1].node.slug
+  questionnaires.forEach(
+    (questionnaire: ContentfulQuestionnaire, index: number) => {
+      const previousQuestionnaire =
+        index === lessons.length - 1 ? null : lessons[index + 1].slug
+      const nextQuestionnaire = index === 0 ? null : lessons[index - 1].slug
+
+      createPage({
+        path: getLocalizedPath(
+          `/questionnaire/${questionnaire.slug}`,
+          questionnaire.node_locale
+        ),
+        component: path.resolve(`./src/templates/questionnaire.tsx`),
+        context: {
+          locale: questionnaire.node_locale,
+          slug: questionnaire.slug,
+          next: nextQuestionnaire,
+          previous: previousQuestionnaire,
+        },
+      })
+    }
+  )
+
+  authors.forEach((author: ContentfulAuthor) => {
+    const scholar = author.slug ? author.slug.replace(/-/g, " ") : "no"
 
     createPage({
-      path: getLocalizedPath(
-        `/questionnaire/${questionnaire.node.slug}`,
-        questionnaire.node.node_locale
-      ),
-      component: path.resolve(`./src/templates/questionnaire.tsx`),
-      context: {
-        locale: questionnaire.node.node_locale,
-        slug: questionnaire.node.slug,
-        next: nextQuestionnaire,
-        previous: previousQuestionnaire,
-      },
-    })
-  })
-
-  authors.forEach((author: Author) => {
-    const scholar = author.node.slug
-      ? author.node.slug.replace(/-/g, " ")
-      : "no"
-
-    createPage({
-      path: getLocalizedPath(
-        `author/${author.node.slug}`,
-        author.node.node_locale
-      ),
+      path: getLocalizedPath(`author/${author.slug}`, author.node_locale),
       component: path.resolve(`./src/templates/author.tsx`),
       context: {
-        slug: author.node.slug,
-        locale: author.node.node_locale,
+        slug: author.slug,
+        locale: author.node_locale,
         scholar: scholar,
       },
     })
--- src/lib/query-contentful.ts
+++ src/lib/query-contentful.ts
@@ -1,3 +1,54 @@
+export const contentfulData = `
+  {
+    allContentfulLesson: allContentfulLesson {
+      nodes {
+        slug
+        node_locale
+      }
+    }
+
+    allContentfulHabit: allContentfulHabit {
+      nodes {
+        slug
+        node_locale
+      }
+    }
+
+    allContentfulWeek: allContentfulWeek(
+      filter: { internal: { type: { eq: "ContentfulWeek" } } }
+    ) {
+      nodes {
+        slug
+        node_locale
+      }
+    }
+
+    allContentfulAuthor: allContentfulAuthor {
+      nodes {
+        slug
+        node_locale
+      }
+    }
+
+    contentfulTagsQuery: allContentfulLesson(
+      filter: { node_locale: { eq: "en-US" } }
+    ) {
+      group(field: keywords) {
+        fieldValue
+        totalCount
+      }
+    }
+
+    allContentfulQuestionnaire: allContentfulQuestionnaire(
+      sort: { fields: questions___id }
+    ) {
+      nodes {
+        slug
+        node_locale
+      }
+    }
+  }
+`
 export const lessonsQuery = `
   {
     allContentfulLesson: allContentfulLesson {
