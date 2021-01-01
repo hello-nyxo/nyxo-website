bab8e655c32d6901aa73a1c724cdac1698a15398
Perttu Lähteenlahti <perttu.lahteenlahti@qvik.fi>
2021-01-01 22:02:01 +0200

    Fix codeblocks

--- gatsby-config.js
+++ gatsby-config.js
@@ -160,6 +160,7 @@ module.exports = {
           // },
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
+          `gatsby-remark-prismjs`,
         ],
       },
     },
